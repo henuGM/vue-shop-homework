c9e63d4ed4d3ea864501a363d6cb8dd001e70561
lysimportant <lysimportant@outlook.com>
2021-09-01 21:33:55 +0800

    fix

--- src/components/home/welcome/Welcome.vue
+++ src/components/home/welcome/Welcome.vue
@@ -6,27 +6,29 @@
       <h4>以前的小东西 :</h4>
       <ul>
         <li>
-          <a href="https://lianghj.top" target="_blank">网易云音乐项目</a>
-          <span>根据API文档模仿的项目</span>
+          <a href="https://lianghj.top" target="_blank">网易云音乐项目</a
+          ><span>根据API文档模仿的项目</span>
         </li>
         <li>
-          <a href="https://blog.csdn.net/weixin_46858768?spm=1000.2115.3001.5343" target="_blank"> 博客</a>
-          <span>CSDN博客 在学习时写的一些笔记</span>
+          <a
+            href="https://blog.csdn.net/weixin_46858768?spm=1000.2115.3001.5343"
+            target="_blank"
+            >博客</a
+          ><span>CSDN博客 在学习时写的一些笔记</span>
         </li>
         <li>
-          <a href="https://github.com/lysimportant" target="_blank">Github</a>
-          <span>Github 项目文件等存放的地方 -- 国外</span>
+          <a href="https://github.com/lysimportant" target="_blank">Github</a
+          ><span>Github 项目文件等存放的地方 -- 国外</span>
         </li>
         <li>
-          <a href="https://gitee.com/lysimportant" target="_blank">Gitte</a>
-          <span>Gitte 项目文件等存放的地方 -- 国内</span>
+          <a href="https://gitee.com/lysimportant" target="_blank">Gitte</a
+          ><span>Gitte 项目文件等存放的地方 -- 国内</span>
         </li>
         <li><hr /></li>
         <li>
-          <a href="http://web.lianghj.top" target="_blank">模仿网站</a>
-          <span
-            >练习写的模仿
-            <a
+          <a href="http://web.lianghj.top" target="_blank">模仿网站</a
+          ><span
+            >练习写的模仿<a
               href="https://isite.baidu.com/site/hailandz.com/3cda6caa-9424-48f6-9bc7-beb530670cb2?fid=nHn1PW0LPH6snHndPHmvrHbdnHKxnWcdg1D&ch=4&bd_vid=8468093689305312712&title=%E9%A6%96%E9%A1%B5&field=&orderBy=&categoryId=undefined&productGroupsId="
               target="_blank"
               >源网站</a
@@ -34,24 +36,28 @@
           >
         </li>
         <li>
-          <a href="http://myweb.lianghj.top" target="_blank">自作网站</a>
-          <span>自写的一个网站</span>
+          <a href="http://myweb.lianghj.top" target="_blank">自作网站</a
+          ><span>自写的一个网站</span>
         </li>
         <li>
-          <a href="http://mall.lianghj.top" target="_blank">coderwhy蘑菇商城</a>
-          <span
-            >在网页打开切换到手机端后要刷新页面才可以滑动,手机端打开可以直接滑动 -> mall.lianghj.top
-            <a href="https://www.bilibili.com/video/BV17j411f74d?t=12">源网站</a>
-          </span>
+          <a href="http://mall.lianghj.top" target="_blank">coderwhy蘑菇商城</a
+          ><span
+            >在网页打开切换到手机端后要刷新页面才可以滑动,手机端打开可以直接滑动
+            -> mall.lianghj.top<a
+              href="https://www.bilibili.com/video/BV17j411f74d?t=12"
+              >源网站</a
+            ></span
+          >
         </li>
         <li>
-          <a href="http://shop.lianghj.top" target="_blank">Pink品优购商城</a>
-          <span
-            >类似购物网站
-            <a href="https://www.bilibili.com/video/BV14J4114768?from=search&seid=6544069264377211049">源网站</a>
-          </span>
+          <a href="http://shop.lianghj.top" target="_blank">Pink品优购商城</a
+          ><span
+            >类似购物网站<a
+              href="https://www.bilibili.com/video/BV14J4114768?from=search&seid=6544069264377211049"
+              >源网站</a
+            ></span
+          >
         </li>
-        <li><hr /></li>
       </ul>
     </div>
     <address>纯自娱练习用</address>
@@ -60,7 +66,7 @@
 
 <script>
 export default {
-  name: 'Welcome',
+  name: 'Welcome'
 }
 </script>
 
